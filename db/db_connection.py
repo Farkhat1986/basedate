9614c77bd01e4c5a03bfecea3914a4d2f82f2e26
Farhat <f190886@mail.ru>
2025-10-13 13:09:57 +0400

    изменение README, создание run_test файла для запуска тестов, а также генерации allure отчетов и открытии localhost с allure отчетом

--- db/db_connection.py
+++ db/db_connection.py
@@ -1,29 +1,36 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
+from contextlib import contextmanager
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "mysql+pymysql://wordpress:wordpress@localhost:3307/wordpress"
-
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
 
+@contextmanager
 def get_session():
-    return Session()
+    session = Session()
+    try:
+        yield session
+    finally:
+        session.close()
 
 def get_post_from_db(post_id: int):
-    session = get_session()
-    query = text("SELECT ID, post_title, post_content, post_status, post_type FROM wp_posts WHERE ID = :id")
-    result = session.execute(query, {"id": post_id}).fetchone()
-    session.close()
-    return result
+    with get_session() as session:
+        return session.execute(
+            text("SELECT ID, post_title, post_content, post_status, post_type FROM wp_posts WHERE ID = :id"),
+            {"id": post_id}
+        ).fetchone()
 
 def get_comment_from_db(comment_id: int):
-    session = get_session()
-    query = text("""
-        SELECT comment_ID, comment_post_ID, comment_content, comment_author, comment_approved
-        FROM wp_comments
-        WHERE comment_ID = :id
-    """)
-    result = session.execute(query, {"id": comment_id}).fetchone()
-    session.close()
-    return result
-
+    with get_session() as session:
+        return session.execute(
+            text("""
+                SELECT comment_ID, comment_post_ID, comment_content, comment_author, comment_approved
+                FROM wp_comments
+                WHERE comment_ID = :id
+            """),
+            {"id": comment_id}
+        ).fetchone()
